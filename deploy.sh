491916f0b1268f0798fedaa10add38ef6ff65316
Roberto Bermejo Martinez <me@robertobermejo.es>
2021-06-07 18:30:29 +0200

    feat: Use codebuild output

--- deploy.sh
+++ deploy.sh
@@ -41,11 +41,6 @@ export AWS_ACCESS_KEY_ID=$INPUT_AWS_ACCESS_KEY
 export AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET_KEY
 export AWS_DEFAULT_REGION=$INPUT_AWS_REGION
 
-if [ "$INPUT_CODEBUILD_ID" ]; then
-  echo "::debug::codebuild_id is present. traying to load s3 folder from that build."
-  aws codebuild batch-get-builds --ids $INPUT_CODEBUILD_ID | jq --raw-output '.builds|first.artifacts.location' | sed -E 's/arn:aws:s3:::([^\/]+)\/(.+)/\2/gm;t'
-fi
-
 function fileExists() {
   aws s3api head-object \
      --bucket "$INPUT_S3_BUCKET" \
@@ -53,22 +48,15 @@ function fileExists() {
      --query ETag --output text > /dev/null 2>&1 && return 1 || return 0
 }
 
-function getS3Folder() {
-  aws codebuild batch-get-builds --ids $INPUT_CODEBUILD_ID | jq --raw-output '.builds|first.artifacts.location' | sed -E 's/arn:aws:s3:::([^\/]+)\/(.+)/\2/gm;t'
-}
-
-function fileExists() {
-  aws s3api head-object \
-     --bucket "$INPUT_S3_BUCKET" \
-     --key "$1" \
-     --query ETag --output text > /dev/null 2>&1 && return 1 || return 0
-}
-
+echo "::debug::codebuild_id $INPUT_CODEBUILD_ID"
 if [ "$INPUT_CODEBUILD_ID" ]; then
   echo "::debug::codebuild_id is present. traying to load s3 folder from that build."
   INPUT_S3_BUCKET=$(aws codebuild batch-get-builds --ids $INPUT_CODEBUILD_ID | jq --raw-output '.builds|first.artifacts.location' | sed -E 's/arn:aws:s3:::([^\/]+)\/(.+)/\1/gm;t')
   INPUT_S3_FOLDER=$(aws codebuild batch-get-builds --ids $INPUT_CODEBUILD_ID | jq --raw-output '.builds|first.artifacts.location' | sed -E 's/arn:aws:s3:::([^\/]+)\/(.+)/\2/gm;t')
+  echo "::set-output name=BUCKET::${INPUT_S3_BUCKET}"
+  echo "::set-output name=FOLDER::${INPUT_S3_BUCKET}"
 fi
+
 while fileExists $INPUT_S3_FOLDER; do
   sleep 5
   echo "::debug::waiting File not exists."
